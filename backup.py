def883ec3a2b665189b6ef6aaf376516ea52a775
asermartinez <aser.martinez@gmail.com>
2018-04-16 13:16:42 +0200

    add .gitignore

--- backup.py
+++ backup.py
@@ -3,12 +3,11 @@
 # Python script to backup multiple mysql databases and folders.
 # to use with my drupal / wordpress sites
 # Tested with Python 3.6.3
-# Requirement: Glob2
-#   pip3 install glob2
+# Requirement: Glob2 => pip3 install glob2
 
-# Create dbnames.txt with a database name per line
-# Create foldernames.txt with a folder per line
-# Set the path variables DB_NAME and FOLDER_NAME
+# Create dbname.txt with a database name per line
+# Create foldername.txt with a folder per line
+# Set the path variables db_name and folder_name
 
 # To avoid passing mysql password to the script, save it in a file in your home folder:
 # ~/.my.cnf
@@ -22,7 +21,6 @@
 # Scripts for reference:
 # bash script https://tecadmin.net/python-script-for-mysql-database-backup/
 # python script http://www.guyrutenberg.com/2010/02/28/improved-ftp-backup-for-wordpress/
-
 
 import os
 import shlex
